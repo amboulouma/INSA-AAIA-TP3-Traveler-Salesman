ccba9c21e9420f295d41c21c2efc7a99598a5887
Amine M. Boulouma <amine@boulouma.com>
2018-05-30 15:24:29 +0200

    Added permut function

--- exo1.c
+++ exo1.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void permut(int vus[], int nbVus, int nonVus[], int nbNonVus){
+void permut(int vus[], int nbVus, int nonVus[], int nbNonVus)
+{
     /*
     Entree :
      - vus[0..nbVus-1] = sommets visites
      - nonVus[0..nbNonVus-1] = sommets non visites
+
     Precondition : nbVus > 0 et vus[0] = 0 (le tour commence toujours par le sommet 0)
+    */
+
+    if (nbNonVus == 0)
+    {
+        for (int i = 0; i < nbVus; i++)
+            printf("%d", vus[i]);
+        printf("\n");
+    }
+
+    for (int i = 0; i < nbNonVus; i++)
+    {
+        vus[nbVus] = nonVus[i];
+        nonVus[i] = nonVus[nbNonVus - 1];
+        permut(vus, nbVus + 1, nonVus, nbNonVus - 1);
+        nonVus[i] = vus[nbVus];
+        vus[nbVus] = 0;
+    }
+
+    /*
     Postcondition : affiche tous les tours commencant par vus[0..nbVus-1] et se terminant par les sommets de nonVus[0..nbNonVus-1] (dans tous les ordres possibles)
      */
     // ECRIVEZ VOTRE CODE ICI :-)
 }
 
-int main(){
+int main()
+{
     int nbSommets, i;
-    scanf("%d",&nbSommets);
-    int vus[nbSommets], nonVus[nbSommets-1];
+    scanf("%d", &nbSommets);
+    int vus[nbSommets], nonVus[nbSommets - 1];
     vus[0] = 0;
-    for (i=0; i<nbSommets-1; i++)
-        nonVus[i] = i+1;
-    permut(vus,1,nonVus,nbSommets-1);
+    for (i = 0; i < nbSommets - 1; i++)
+        nonVus[i] = i + 1;
+    permut(vus, 1, nonVus, nbSommets - 1);
 }
