5e2f3a61a85ad19d43f95c6d06c94de932e89aa1
Amine M. Boulouma <amine@boulouma.com>
2018-05-30 15:38:22 +0200

    Added permut function

--- exo2.c
+++ exo2.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** cout;
+int **cout;
 
-void creeCout(int nbSommets){
+void creeCout(int nbSommets)
+{
     int coutMin = 10;
     int coutMax = 40;
     int i, j, iseed, it;
     iseed = 1;
-    cout = (int**) malloc(nbSommets*sizeof(int*));
-    for (i=0; i<nbSommets; i++){
-        cout[i] = (int*) malloc(nbSommets*sizeof(int));
-        for (j=0; j<nbSommets; j++){
-            if (i == j) cout[i][j] = coutMax+1;
-            else {
+    cout = (int **)malloc(nbSommets * sizeof(int *));
+    for (i = 0; i < nbSommets; i++)
+    {
+        cout[i] = (int *)malloc(nbSommets * sizeof(int));
+        for (j = 0; j < nbSommets; j++)
+        {
+            if (i == j)
+                cout[i][j] = coutMax + 1;
+            else
+            {
                 it = 16807 * (iseed % 127773) - 2836 * (iseed / 127773);
-                if (it > 0)    iseed = it;
-                else iseed = 2147483647 + it;
-                cout[i][j] = coutMin + iseed % (coutMax-coutMin+1);
+                if (it > 0)
+                    iseed = it;
+                else
+                    iseed = 2147483647 + it;
+                cout[i][j] = coutMin + iseed % (coutMax - coutMin + 1);
             }
         }
     }
 }
 
-void permut(int vus[], int nbVus, int nonVus[], int nbNonVus, int longueur){
+void permut(int vus[], int nbVus, int nonVus[], int nbNonVus, int longueur)
+{
     /*
     Variable globale :
      - pour tout couple de sommets (i,j), cout[i][j] = cout pour aller de i vers j
@@ -33,17 +41,40 @@ void permut(int vus[], int nbVus, int nonVus[], int nbNonVus, int longueur){
     Precondition :
      - nbVus > 0 et Vus[0] = 0 (on commence toujours par le sommet 0)
      - longueur = somme des couts des arcs du chemin <vus[0], vus[1], ..., vus[nbVus-1]>
-    Postcondition : affiche les longueurs de tous les circuits commençant par vus[0..nbVus-1] et se terminant par les sommets de nonVus[0..nbNonVus-1] (dans tous les ordres possibles), suivis de 0
-     */
+    */
+
+    int sum = 0;
+
+    if (nbNonVus == 0)
+    {
+        for (int i = 0; i < nbVus - 1; i++)
+            sum += cout[i][i + 1];
+        printf("%d\n", sum);
+    }
+
+    for (int i = 0; i < nbNonVus; i++)
+    {
+        vus[nbVus] = nonVus[i];
+        nonVus[i] = nonVus[nbNonVus - 1];
+        permut(vus, nbVus + 1, nonVus, nbNonVus - 1, longueur);
+        nonVus[i] = vus[nbVus];
+        vus[nbVus] = 0;
+    }
+
+    /*
+    Postcondition:
+    affiche les longueurs de tous les circuits commençant par vus[0..nbVus - 1] et se terminant par les sommets de nonVus[0..nbNonVus - 1](dans tous les ordres possibles), suivis de 0 */
 }
 
-int main(){
+int main()
+{
     int nbSommets;
-    scanf("%d",&nbSommets);
+    scanf("%d", &nbSommets);
     creeCout(nbSommets);
     int vus[nbSommets];
-    int nonVus[nbSommets-1];
-    for (int i=0; i<nbSommets-1; i++) nonVus[i] = i+1;
+    int nonVus[nbSommets - 1];
+    for (int i = 0; i < nbSommets - 1; i++)
+        nonVus[i] = i + 1;
     vus[0] = 0;
-    permut(vus,1,nonVus,nbSommets-1,0);
+    permut(vus, 1, nonVus, nbSommets - 1, 0);
 }
